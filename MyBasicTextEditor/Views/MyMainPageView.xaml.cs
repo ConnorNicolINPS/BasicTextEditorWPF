bc2ad1fde1e6ab05811315935f2157ffd5a23329
andrew.rae <andrew.rae@INPSESPC214>
2016-09-12 13:49:11 +0100

    Updated file such that it will replace text in the headers and footers.

--- MyBasicTextEditor/Views/MyMainPageView.xaml.cs
+++ MyBasicTextEditor/Views/MyMainPageView.xaml.cs
@@ -23,7 +23,7 @@ namespace MyBasicTextEditor
 
         private void GetTemplates()
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\conor.nicol\Desktop\TestTemplates\");
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\andrew.rae\Desktop\TestTemplates\");
             var templates = dir.GetFiles("*.dotx", SearchOption.AllDirectories);
 
             if (templates != null)
@@ -53,7 +53,7 @@ namespace MyBasicTextEditor
             {
                 rng.Font.Name = "Georgia";
                 rng.InsertAfter(new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd).Text);
-                object filename = @"C:\Users\conor.nicol\Desktop\MyWord.doc";
+                object filename = @"C:\Users\andrew.rae\Desktop\MyWord.doc";
                 adoc.SaveAs(ref filename);
                 WordApp.Visible = true;
             }
@@ -76,25 +76,51 @@ namespace MyBasicTextEditor
             var currentViewModel = this.ViewModel as MyMainPageViewModel;
             if (currentViewModel.SelectedTemplate != null)
             {
-                object templatePath = @"C:\Users\conor.nicol\Desktop\TestTemplates\" + currentViewModel.SelectedTemplate + ".dotx";
-                string saveFilePath = @"C:\Users\conor.nicol\Desktop\TestTemplates\";
+                object templatePath = @"C:\Users\andrew.rae\Desktop\TestTemplates\" + currentViewModel.SelectedTemplate + ".dotx";
+                string saveFilePath = @"C:\Users\andrew.rae\Desktop\TestTemplates\";
 
                 wordDoc = WordApp.Documents.Add(templatePath); // open the template
                 wordDoc.SaveAs(saveFilePath + "TestBPLetter" + currentViewModel.SelectedPatient.Forename); // save template as document so you ont overwirte the template
                 wordDoc = WordApp.Documents.Open(saveFilePath + "TestBPLetter" + currentViewModel.SelectedPatient.Forename + ".docx"); // open the newly saved doc to read the text
 
                 Range rng = wordDoc.Range();
+                int count = wordDoc.Sections.Count;
+                Range headerRange = wordDoc.Sections[1].Headers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                Range footerRange = wordDoc.Sections[count].Footers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
 
-                Find findObject = rng.Find;
+
+                Find documentFindObject = rng.Find;
+                Find headerFindObject = headerRange.Find;
+                Find footerFindObject = footerRange.Find;
+
                 object replaceAll = WdReplace.wdReplaceAll;
 
                 foreach (Tags tag in currentViewModel.TagList)
                 {
-                    findObject.ClearFormatting();
-                    findObject.Text = tag.Tag;
-                    findObject.Replacement.ClearFormatting();
-                    findObject.Replacement.Text = currentViewModel.ReplaceTag(tag.Tag);
-                    findObject.Execute(Replace: replaceAll);
+                    //// Clear search box
+                    documentFindObject.ClearFormatting();
+                    headerFindObject.ClearFormatting();
+                    footerFindObject.ClearFormatting();
+
+                    //// Search for tag
+                    documentFindObject.Text = tag.Tag;
+                    headerFindObject.Text = tag.Tag;
+                    footerFindObject.Text = tag.Tag;
+
+                    //// Clear search tags
+                    documentFindObject.Replacement.ClearFormatting();
+                    headerFindObject.Replacement.ClearFormatting();
+                    footerFindObject.Replacement.ClearFormatting();
+
+                    //// Set up text to replace with
+                    documentFindObject.Replacement.Text = currentViewModel.ReplaceTag(tag.Tag);
+                    headerFindObject.Replacement.Text = currentViewModel.ReplaceTag(tag.Tag);
+                    footerFindObject.Replacement.Text = currentViewModel.ReplaceTag(tag.Tag);
+
+                    //// Run replacement
+                    documentFindObject.Execute(Replace: replaceAll);
+                    headerFindObject.Execute(Replace: replaceAll);
+                    footerFindObject.Execute(Replace: replaceAll);
                 }
             }
             else
